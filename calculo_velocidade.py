7c1e9705c3edf1d7d21ebfec667da5fb186a2618
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2021-04-18 12:49:06 -0300

    update file

--- calculo_velocidade.py
+++ calculo_velocidade.py
@@ -1,14 +1,14 @@
 velocidade_cap = int(input("Informe a velocidade capturada: "))
 limite_velocidade_via = int(input("Informe a velocidade permitida da via: "))
-placa_veiculo = str(input("Informe a placa do veiculo, sem o hifen: "))
+placa_veiculo = str(input("Informe a placa do veiculo: "))
 velocidade_final = round(int(limite_velocidade_via * velocidade_cap)) / 100.0
 
 if velocidade_cap > limite_velocidade_via:
     if velocidade_final < 20.00:
-        print(f'Veiculo {placa_veiculo} - Infracao media - valor: R$ 130,16.')
+        print(f'{placa_veiculo} - {velocidade_cap} km/h - Media R$ 130,16.')
     elif velocidade_final < 50.00:
-        print(f'Veiculo {placa_veiculo} - Infracao grave - valor: R$ 195,23.')
+        print(f'{placa_veiculo} - {velocidade_cap} km/h- Grave R$ 195,23.')
     elif velocidade_final > 51.00:
-        print(f'Veiculo {placa_veiculo} - Infracao gravissima - valor: R$ 880,41.')
+        print(f'{placa_veiculo} - {velocidade_cap} km/h - Gravissima R$ 880,41.')
 else:
-    print(f'Veiculo {placa_veiculo} - Velocidade dentro do permitido.')
+    print(f'Veiculo {placa_veiculo} - {velocidade_cap} km/h - Permitido.')
